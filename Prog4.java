af3f9ae71f76f83887a367d76712772d769089a0
HardikShah563 <hardikts@gmail.com>
2023-08-19 22:46:23 +0530

    prog 4

---
+++ Prog4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Prog4 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the temperature in farhenite scale: ");
+        double farhenite = in.nextDouble();
+        System.out.println("Temperature in celsius is: " + ((farhenite - 32) / 1.8));
+    }
+}
