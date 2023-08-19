2c246098b4806e827fa906b68d98ccfec1787c84
HardikShah563 <hardikts@gmail.com>
2023-08-19 22:12:03 +0530

    prog 1

---
+++ Prog1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Prog1 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        boolean flag = false;
+        System.out.println("Enter your marks in maths: ");
+        int mathsMarks = in.nextInt();
+        System.out.println("Enter your marks in physics: ");
+        int phyMarks = in.nextInt();
+        System.out.println("Enter your marks in chemistry: ");
+        int chemMarks = in.nextInt();
+        if(mathsMarks + phyMarks >= 150) {
+            flag = true;
+        }
+        
+        if(mathsMarks >= 60 && phyMarks >= 50 && chemMarks >= 40) {
+            flag = true;
+        }
+        
+        if(flag) {
+            System.out.println("You are admitted into college");
+        }
+        else {
+            System.out.println("You are not admitted into college");
+        }
+    }
+}
