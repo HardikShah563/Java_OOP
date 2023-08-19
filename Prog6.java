f95f6fa96ff74c3e32c7845da5c3a04ee108e5c2
HardikShah563 <hardikts@gmail.com>
2023-08-19 23:07:34 +0530

    prog 6

---
+++ Prog6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Prog6 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = in.nextInt();
+        int d = 0, sum = 0;
+        while(num != 0) {
+            d = num % 10;
+            sum += d;
+            num =  num / 10;
+        }
+        System.out.println("The sum of all the digits in the number is: " + sum);
+    }
+}
